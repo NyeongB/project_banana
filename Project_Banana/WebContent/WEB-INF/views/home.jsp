aa2e73d592afb77a19b32667c3ca2ad84a4daabf
NyeongB <kdtoaj14@ajou.ac.kr>
2020-06-21 22:20:42 +0900

    최초 환경 구축

---
+++ Project_Banana/WebContent/WEB-INF/views/home.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>메인페이지</title>
+</head>
+<body>
+
+<div>
+	<h1>home.jsp</h1>
+	<!-- http://localhost:8090/Project_Banana
+	홈페이지 주소 요청시 최초로 보여지는 페이지  -->
+</div>
+
+</body>
+</html>
---
+++ Project_Banana/src/com/test/mvc/HomeController.java
@@ -0,0 +1,21 @@
+package com.test.mvc;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class HomeController implements Controller{
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		ModelAndView mav = new ModelAndView();
+		
+		mav.setViewName("/WEB-INF/views/home.jsp");
+		
+		return mav;
+	}
+
+}
