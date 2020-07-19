f1ff0561876dafd5947b14b9f792d7cb2f78729b
jinyoung1704 <jinyoung1704@naver.com>
2020-07-19 12:28:05 +0900

    파일생성

--- Project_Banana/src/com/banana/controller/MemberController.java
+++ Project_Banana/src/com/banana/controller/MemberController.java
@@ -393,7 +393,7 @@ public class MemberController
 		String view = null; 
 		
 		
-		view = "/WEB-INF/groupbuying/GroupBuyingPostOffer.jsp";
+		view = "/GroupBuyingPostOffer.jsp";
 		
 		
 		return view;
