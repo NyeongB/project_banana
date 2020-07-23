19283039bf27f785ce407f7f25180b678b409e46
NyeongB <kdtoaj14@ajou.ac.kr>
2020-07-23 15:32:59 +0900

    포인트 충전 완료

---
+++ Project_Banana/src/com/banana/my/IPointChargeDAO.java
@@ -0,0 +1,7 @@
+package com.banana.my;
+
+public interface IPointChargeDAO
+{
+	// 충전 메소드 
+	public void pointCharge(PointChargeDTO dto);
+}
---
+++ Project_Banana/src/com/banana/my/PointChargeDTO.java
@@ -0,0 +1,36 @@
+package com.banana.my;
+
+public class PointChargeDTO
+{
+	private String b_user_code, point, type;
+
+	public String getB_user_code()
+	{
+		return b_user_code;
+	}
+
+	public void setB_user_code(String b_user_code)
+	{
+		this.b_user_code = b_user_code;
+	}
+
+	public String getPoint()
+	{
+		return point;
+	}
+
+	public void setPoint(String point)
+	{
+		this.point = point;
+	}
+
+	public String getType()
+	{
+		return type;
+	}
+
+	public void setType(String type)
+	{
+		this.type = type;
+	}
+}
