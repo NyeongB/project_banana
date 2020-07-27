47502e76da1f0fc6b069429e856f82c6353a4227
ilfm <dnehd1828@gmail.com>
2020-07-28 01:42:34 +0900

    찜 페이징 완료 _김동휘

--- Project_Banana/src/com/banana/controller/UserMyJJimController.java
+++ Project_Banana/src/com/banana/controller/UserMyJJimController.java
@@ -12,9 +12,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.banana.groupbuying.GPostDTO;
 import com.banana.my.IMyReviewDAO;
 import com.banana.my.IUserMyJJimDAO;
 import com.banana.my.MyReviewDTO;
+import com.banana.util.Paging;
 import com.banana.util.SessionInfo;
 
 @Controller
@@ -28,9 +30,11 @@ public class UserMyJJimController
 	public String list(Model model, HttpServletRequest request) // 나중에 session 으로 받기
 	{
 		String view = null;
+	
 
 		try
 		{	
+			
 			// 세션 받아오기
 			HttpSession session = request.getSession();
 			SessionInfo info = (SessionInfo) session.getAttribute("user");
@@ -43,8 +47,30 @@ public class UserMyJJimController
 			String b_user_code = info.getB_user_code();
 			
 			IUserMyJJimDAO dao = SqlSession.getMapper(IUserMyJJimDAO.class);
-			model.addAttribute("jjim", dao.jjimList(b_user_code));			
+			
+			// 리스트의 총페이지를 가져오는 메소드 
+			int count = dao.getCount(b_user_code);
+			
+			// 두개 반드시 선언
+			Paging paging = new Paging();
+			String pageNum = request.getParameter("pageNum");
+			
+			//테이블에서 가져올 리스트의 시작과 끝 위치
+			int start = paging.getStart(pageNum,count );
+			int end = paging.getEnd(pageNum, count);
+			
+			// 페이지번호를 받아온 
+			String pageIndexList = paging.pageIndexList(pageNum, count);
+			
+			// DTO에 넣기 
+			GPostDTO dto = new GPostDTO();
+			dto.setB_user_code(b_user_code);
+			dto.setStart(start);
+			dto.setEnd(end);
+			
+			model.addAttribute("jjim", dao.jjimList(dto));			
 			model.addAttribute("nickName", nickName);	
+			model.addAttribute("pageIndexList", pageIndexList);
 			
 		} catch (Exception e)
 		{
