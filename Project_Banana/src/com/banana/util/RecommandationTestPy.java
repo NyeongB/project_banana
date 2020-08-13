d8ca88fd204fdc194afa728e85cf28a299a4e4e8
NyeongB <kdtoaj14@ajou.ac.kr>
2020-08-13 12:45:57 +0900

    추천 엔진 구현(python)

--- Project_Banana/src/com/banana/util/RecommandationTestPy.java
+++ Project_Banana/src/com/banana/util/RecommandationTestPy.java
@@ -11,26 +11,7 @@ public class RecommandationTestPy
 	{
 
 		interpreter = new PythonInterpreter();
-		interpreter.exec("import cx_Oracle as oci");
-		interpreter.exec("import numpy as np");
-		interpreter.exec("import pandas as pd");
-		interpreter.exec("from IPython.display import display");
-		interpreter.exec("import json");
-		interpreter.exec("");
-		interpreter.exec("import sys");
-		interpreter.exec("");
-		interpreter.exec("");
-		interpreter.exec("# Oracle 서버와 연결(Connection 맺기)");
-		interpreter.exec("conn = oci.connect('13.231.82.47:1521/xe')");
-		interpreter.exec("");
-		interpreter.exec("# Connection 확인");
-		interpreter.exec("print(conn.version)");
-		interpreter.exec("");
-		interpreter.exec("");
-		interpreter.exec("");
-		interpreter.exec("");
-		interpreter.exec("");
-		interpreter.exec("");
+		interpreter.exec("print('tets')");
 
 	}
 
