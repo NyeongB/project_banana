c2be59b13c65a6020359fae20df564893418e7de
NyeongB <kdtoaj14@ajou.ac.kr>
2020-08-25 08:56:25 +0900

    포인트충전 수정

--- Project_Banana/src/com/banana/util/RecommandationTestPy.java
+++ Project_Banana/src/com/banana/util/RecommandationTestPy.java
@@ -2,16 +2,19 @@ package com.banana.util;
 
 import org.python.util.PythonInterpreter;
 
+
+
 public class RecommandationTestPy
 {
 
-	private static PythonInterpreter interpreter;
 
 	public static void main(String[] args)
 	{
 
-		interpreter = new PythonInterpreter();
-		interpreter.exec("print('tets')");
+		System.setProperty("python.cachedir.skip", "true");
+        PythonInterpreter interpreter = new PythonInterpreter();
+        interpreter.execfile("RecommandSimilarUser.py");
+        interpreter.exec("print('test')");
 
 	}
 
